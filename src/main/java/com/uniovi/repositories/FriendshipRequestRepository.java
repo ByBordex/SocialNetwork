fe51c050e69e30a2fe83b767fbe85efc726e8088
Víctor Suárez Fernández <vicsufer@gmail.com>
2018-03-05 15:43:49 +0100

    Full support for accepting requests

--- src/main/java/com/uniovi/repositories/FriendshipRequestRepository.java
+++ src/main/java/com/uniovi/repositories/FriendshipRequestRepository.java
@@ -1,10 +1,14 @@
 package com.uniovi.repositories;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.uniovi.entites.FriendshipRequest;
 import com.uniovi.entites.User;
@@ -15,6 +19,11 @@ public interface FriendshipRequestRepository extends CrudRepository<FriendshipRe
 	@Query("SELECT r FROM FriendshipRequest r WHERE r.receiver = ?1 AND r.accepted = false ORDER BY r.id ASC")
 	Page<FriendshipRequest> findPendingRequestToUser(Pageable pageable, User receiver);
 
-	@Query("UPDATE FriendshipRequest SET accept = 1 WHERE id=?1 and receiver=?2")
-	void acceptRequest(Long id,User receiver  );
+	@Query("SELECT r FROM FriendshipRequest r WHERE r.sender = ?1 AND r.accepted = false ORDER BY r.id ASC")
+	List<FriendshipRequest> findPendingRequestFromUser(User sender);
+	
+	@Transactional
+	@Modifying
+	@Query("UPDATE FriendshipRequest SET accepted = true WHERE id=?1 AND receiver = ?2")
+	void acceptRequest(Long id, User receiver);
 }
