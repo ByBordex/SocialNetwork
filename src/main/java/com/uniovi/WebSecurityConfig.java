d86990d351405f5e8a0fb04c98cfc193a174244e
Víctor Suárez Fernández <vicsufer@gmail.com>
2018-03-05 11:48:51 +0100

    Cambios a la UI

--- src/main/java/com/uniovi/WebSecurityConfig.java
+++ src/main/java/com/uniovi/WebSecurityConfig.java
@@ -25,7 +25,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests().antMatchers("/css/**", "/img/**", "/script/**", "/", "/signup")
-				.permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll()
+				.permitAll().anyRequest().authenticated()
+				.and().formLogin().loginPage("/login").permitAll()
 				.defaultSuccessUrl("/home").failureUrl("/login.html?error=true").and().logout().permitAll();
 	}
 
